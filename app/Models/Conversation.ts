be28810537cb75baaabb5cbd23fc4a41fcaec21c
TigorCoding <146649987+TigorCoding@users.noreply.github.com>
2025-07-17 11:39:07 +0700

    add models
    Conversation and Message

--- app/Models/Conversation.ts
+++ app/Models/Conversation.ts
@@ -1,9 +1,20 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Message from './Message'
+import { HasMany } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Conversation extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public sessionId: string
+
+  @column()
+  public lastMessageId: number | null
+
+  @hasMany(() => Message, { foreignKey: 'conversationId' })
+  public messages: HasMany<typeof Message>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
--- app/Models/Message.ts
+++ app/Models/Message.ts
@@ -1,10 +1,27 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Conversation from './Conversation'
 
 export default class Message extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public conversationId: number
+
+  @column()
+  public senderType: string
+
+  @column()
+  public message: string
+
+  @column()
+  public additionalMessage: string | null
+
+  @belongsTo(() => Conversation, { foreignKey: 'conversationId' })
+  public conversation: BelongsTo<typeof Conversation>
+  
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
